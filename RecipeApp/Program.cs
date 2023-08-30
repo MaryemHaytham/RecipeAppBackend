ae1d214db2a68910ef61d859f6e18a14f21f2dbf
sarahyasser <sarahyasserxo@gmail.com>
2023-08-30 18:24:29 +0300

    adding Cors

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -22,4 +22,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+});
+
 app.Run();
