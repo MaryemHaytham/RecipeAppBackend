80ec62738e2f80f8bbc32098db7461d8e69df411
MaryemHaytham <maryemhaythamm@gmail.com>
2023-09-21 17:49:59 +0300

    register edit

---
+++ RecipeApp/ViewModels/AddRecipeVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApp.ViewModels
+{
+    public class AddRecipeVM
+    {
+        [MinLength(4)] // Enforce minimum length of 4 characters
+        public string RecipeName { get; set; }
+
+        [Required]
+        [MinLength(1)] // Enforce minimum length of 1 character
+        public string Ingredients { get; set; }
+
+        [Required]
+        [MinLength(4)] // Enforce minimum length of 1 character
+        public string Steps { get; set; }
+
+        public string? Image { get; set; }
+
+
+        public int UserId { get; set; } // Foreign key
+    }
+}
