11503afd24a459bf1e7700b53053405979ff795c
MaryemHaytham <maryemhaythamm@gmail.com>
2023-09-24 10:00:20 +0300

    Login

---
+++ RecipeAppBLL/Services/IService/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using RecipeAppDAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeAppBLL.Services.IService
+{
+    public interface IJwtService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ RecipeAppBLL/Services/JwtService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RecipeAppBLL.Services.IService;
+using RecipeAppDAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeAppBLL.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Token:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString())
+            }),
+                Expires = DateTime.UtcNow.AddHours(1), // Set your desired expiration time
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
